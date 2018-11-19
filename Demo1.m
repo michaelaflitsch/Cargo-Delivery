7aa1c1ca713fb39fc561f383469598057e1e2e8d
Mark Nussbaum <mark.nussbaum@my.wheaton.edu>
2018-11-19 13:54:21 -0600

    add line to matlab script

--- Demo1.m
+++ Demo1.m
@@ -4,8 +4,7 @@ a=(5+3)- (2+1)  % addition and subtraction
 b=(5*2) + (8/4)  % multiplication
 c=2^(-1) % exponentiation
 d= a+b/c ;  %% Order of operations (),^,*/,+- semi-colon suppresses output
-
-
+e = a^2; 
 
 
 
